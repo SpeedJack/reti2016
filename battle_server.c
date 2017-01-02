f0e2c8c47eb280e9059fc2287db974e086f30b4d
Niccolò Scatena <SpeedJack95@gmail.com>
2017-01-02 23:58:12 +0100

    main function basic code

--- battle_server.c
+++ battle_server.c
@@ -1,27 +1,71 @@
 #include <errno.h>
-#include <stdio.h>
+#include <netdb.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
-void print_error(const char *funcname, const char *format, ...)
-{
-	va_list args;
+#define MIN_PORT_NUMBER	1
+#define MAX_PORT_NUMBER	65535
+#define LISTEN_BACKLOG	20
 
-	va_start(args, format);
+#define _STRINGIZE(x)	#x
 
-	vfprintf(stderr, format, args);
-	fprintf(stderr, "\n");
+#define STRINGIZE(x)	_STRINGIZE(x)
 
-	if (errno)
-		fprintf(stderr, "%s(%d): %s\n",
-				(funcname && *funcname != '\0') ? funcname : "error",
-				errno,
-				strerror(errno));
+#define PRINT_LOCATION	fprintf(stderr, "%s:%s: %s(): ", __FILE__, \
+		STRINGIZE(__LINE__), __func__);
 
-	va_end(args);
-}
+#define PRINT_ERROR(msg)	if (errno) perror(msg); else \
+		do { fprintf(stderr, msg); fprintf(stderr, "\n"); } while (0)
+
+#define handle_error(msg)	do { PRINT_LOCATION PRINT_ERROR(msg); \
+		exit(EXIT_FAILURE); } while (0)
 
 int main(int argc, char **argv)
 {
-	return 0;
+	struct addrinfo hints;
+	struct addrinfo *result, *rp;
+	int s, sfd;
+
+	if (argc != 2) {
+		printf("Usage: %s <port>\n", argv[0]);
+		exit(EXIT_SUCCESS);
+	}
+
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE | AI_V4MAPPED | AI_ADDRCONFIG;
+
+	if ((s = getaddrinfo(NULL, argv[1], &hints, &result)) != 0)
+		handle_error(gai_strerror(s));
+
+	for (rp = result; rp; rp = rp->ai_next) {
+		sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+		if (sfd == -1)
+			continue;
+
+		if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0 &&
+				listen(sfd, LISTEN_BACKLOG) == 0)
+			break;
+
+		close(sfd);
+	}
+	if (!rp)
+		handle_error("Could not create socket");
+
+	freeaddrinfo(result);
+
+	printf("Server listening\n");
+
+	close(sfd);
+	exit(EXIT_SUCCESS);
+exit_free_sock:
+	close(sfd);
+	exit(EXIT_FAILURE);
+	return EXIT_FAILURE;
 }
